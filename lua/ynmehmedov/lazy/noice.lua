51b3affbd5e905cd1fbdd0bc53d87fbfe1e34738
Yuzdzhan Mehmedov <ynmehmedov@superhosting.bg>
2024-02-29 10:30:36 +0200

    Correction

--- lua/ynmehmedov/lazy/noice.lua
+++ lua/ynmehmedov/lazy/noice.lua
@@ -9,8 +9,26 @@ return {
                 filter = {},
             },
         },
+        cmdline = {
+            view = "cmdline",
+        },
+        -- you can enable a preset for easier configuration
         presets = {
-            lsp_doc_border = true
+            bottom_search = true,         -- use a classic bottom cmdline for search
+            command_palette = false,      -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true,        -- add a border to hover docs and signature help
+        },
+        routes = {
+            {
+                filter = {
+                    event = "msg_show",
+                    kind = "",
+                    find = "written",
+                },
+                opts = { skip = true },
+            },
         },
     },
 
